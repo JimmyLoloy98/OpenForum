759b6e2105e581e985c7e6ab225c12e0def4d471
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-19 16:26:22 -0500

    FIX: auto format on save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,34 +8,31 @@ import Login from "../pages/Login";
 import Discussion from "../pages/Discussion";
 import DisplayDiscussion from "../pages/DisplayDiscussion";
 
-
-const linkStyles = {
-  color: "#ffffffcc",
-  transition: "color 0.15s",
-  lineHeight: "24px",
-  _hover: { color: "#ffffff" },
-};
-
 function App() {
-
-  let user = null
-  
-  useEffect(() => {
-    if(!localStorage.getItem("estado")){
-      localStorage.setItem("estado",JSON.stringify(globalState))
-    }
-  });
+  let user = null;
 
   return (
-    <ChakraProvider theme={theme}> 
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={!user ? <Navigate to="login"/> : <Home />} />
-        <Route path="/login" element={!user ?<Login />:<Navigate to="/"/>}></Route>
-        <Route path="/discussion" element={!user ? <Navigate to="/login" /> : <Discussion />}></Route>
-        <Route path="/discussion/:id" element={!user ? <Navigate to="/login"/> :<DisplayDiscussion />}></Route>
-      </Routes>
-     </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={!user ? <Navigate to="login" /> : <Home />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          ></Route>
+          <Route
+            path="/discussion"
+            element={!user ? <Navigate to="/login" /> : <Discussion />}
+          ></Route>
+          <Route
+            path="/discussion/:id"
+            element={!user ? <Navigate to="/login" /> : <DisplayDiscussion />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }

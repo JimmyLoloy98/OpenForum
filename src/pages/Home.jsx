ab8747c0fc52ce82ed3c2d7b4a1b508bad6afdf0
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-19 16:27:16 -0500

    ADD: modal create discussion in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,89 @@
-import { Box , Heading } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+} from "@chakra-ui/react";
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
   return (
     <Box>
-      <Heading>Welcome to Chaos Home</Heading>
+      <Navbar />
+
+      <Modal
+        size={"xl"}
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader fontSize={"4xl"} fontWeight={"normal"}>
+            New Discussion
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Input
+              ref={initialRef}
+              placeholder="Discussion Title"
+              autoFocus
+              focusBorderColor="orange.400"
+            />
+
+            <Textarea
+              rows={5}
+              placeholder="Body Title"
+              mt={6}
+              focusBorderColor="orange.400"
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="orange" mr={3} w={"100%"}>
+              Create Discussion
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Box
+        onClick={onOpen}
+        position={"fixed"}
+        right={"25px"}
+        bottom={"25px"}
+        textAlign={"end"}
+        border={"none"}
+      >
+        <Button
+          // onClick={onOpen}
+          colorScheme="orange"
+          borderRadius={"50%"}
+          w={"55px"}
+          h={"55px"}
+          fontSize={"22px"}
+        >
+          <AddIcon />
+        </Button>
+      </Box>
     </Box>
   );
 }

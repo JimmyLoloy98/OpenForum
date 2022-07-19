571ca3c563e980a8bfe3473c6cf4f6bc5c8dd381
Pavel Leonardo Mansilla Goyas <pavel.mansilla@gmail.com>
2022-07-19 14:46:49 -0500

    rutas reestringidas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
+import React, { useState, useEffect } from 'react';
 import { Box, ChakraProvider, Heading } from "@chakra-ui/react";
 import Navbar from "./Navbar";
 import theme from "./theme";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Discussion from "../pages/Discussion";
@@ -16,14 +17,23 @@ const linkStyles = {
 };
 
 function App() {
+
+  let user = null
+  
+  useEffect(() => {
+    if(!localStorage.getItem("estado")){
+      localStorage.setItem("estado",JSON.stringify(globalState))
+    }
+  });
+
   return (
     <ChakraProvider theme={theme}> 
      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />}></Route>
-        <Route path="discussion" element={<Discussion />}></Route>
-        <Route path="discussion/:id" element={<DisplayDiscussion />}></Route>
+        <Route path="/" element={!user ? <Navigate to="login"/> : <Home />} />
+        <Route path="/login" element={!user ?<Login />:<Navigate to="/"/>}></Route>
+        <Route path="/discussion" element={!user ? <Navigate to="/login" /> : <Discussion />}></Route>
+        <Route path="/discussion/:id" element={!user ? <Navigate to="/login"/> :<DisplayDiscussion />}></Route>
       </Routes>
      </BrowserRouter>
     </ChakraProvider>
